ae52748f0750ad4d1b97c705435b91508628991b
r0nny8000 <r0nny8000@bea2b.de>
2023-01-24 07:36:51 +0100

    added basic functionality

--- workstream/workstream.py
+++ workstream/workstream.py
@@ -1,5 +1,19 @@
 import os
 
-def createFolder(name):
-    print("created folder " + name)
-    print(os.getloadavg())
+
+
+
+def createWorkstreamFolder(home, name):
+    workstream_folder = home  + '/' + name
+    if os.path.exists(workstream_folder):
+        print("Workstram folder already exists.")
+    else:
+        os.mkdir(workstream_folder)
+
+
+def createSymLink(home, links, name):
+    if os.path.islink(links + '/' + name):
+        print("Link already exists.")
+    else:
+        os.symlink(home + '/' + name, links + '/' + name)
+    
--- workstream/ws.py
+++ workstream/ws.py
@@ -1,9 +1,27 @@
 import fire
-from workstream import createFolder
+import os
+
+from workstream import createWorkstreamFolder
+from workstream import createSymLink
+
+HOME = os.path.expanduser('~/Documents')
+LINKS = os.path.expanduser('~/Desktop')
 
 def create(name):
-    print("creating a new workstream with the name " + name + "...")
-    createFolder(name)
+    print("Prechecks...")
+    if not os.path.exists(HOME):
+        print("Folder for workstreams does not exist.")
+        return
+
+    if not os.path.exists(LINKS):
+        print("Folder for symbolic links does not exist.")
+        return
+
+    print("done.")
+
+    print("Creating a new workstream with the name " + name + "...")
+    createWorkstreamFolder(HOME, name)
+    createSymLink(HOME, LINKS, name)
     print("done.")
 
 def _main():
