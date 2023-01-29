fde5113011423e0817ddb97274e4c4f1301bb7d5
r0nny8000 <r0nny8000@bea2b.de>
2023-01-29 22:26:05 +0100

    added readme file creation

--- workstream/workstream.py
+++ workstream/workstream.py
@@ -15,7 +15,18 @@ def createSymLink(workstreams, links, name):
         print("Link already exists.")
     else:
         os.symlink(workstreams + '/' + name, links + '/' + name)
-    
+
 
 def createReadme(workstreams, name):
-    return
+    try:
+        with open(workstreams + '/' + name + '/README.markdown', 'x', encoding="utf-8") as readme:
+            readme.write(name)
+            underline = '\n'
+            for n in name:
+                underline += '='
+            readme.write(underline)
+    except FileNotFoundError:
+        print("Folder " + workstreams + '/' + name + " does not exist.")
+    except FileExistsError:
+        print("Readme file already exists.")
+    
